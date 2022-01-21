62155bbebf9f13372020be14fb3d0e1f2b87a5e2
Altherenz <71811403+Altherenz@users.noreply.github.com>
2022-01-21 19:57:45 +0400

    Create index.php

---
+++ social/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Welcome  To Fakehub - Sign up, Log in, Post </title>
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+	</head>
+
+<body>
+
+	<div id="header">
+		<div class="head-view">
+			<ul>
+				<li><b>Fakehub</b></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li><a href="signin.php" title="Sign in"><button class="btn-sign-in" value="Sign in">Sign in</button></a></li>
+				<li><a href="signup.php" title="Sign up"><button class="btn-sign-up" value="Sign up">Sign up</button></a></li>
+			</ul>
+		</div>
+	</div>
+
+
+</body>
+
+</html>
